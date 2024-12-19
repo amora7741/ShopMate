0aa149b02892d192a357de910504fc1754371174
amora7741 <mora7741@gmail.com>
2024-12-19 14:13:34 -0800

    Setup home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,21 @@
-import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to ShopMate!</h1>
-      <UserButton />
-    </div>
+    <main className="flex h-[80vh] flex-col items-center justify-center gap-4">
+      <h1 className="text-5xl font-bold">ShopMate</h1>
+      <p>
+        A powerful and user-friendly online tool designed to help you keep track
+        of all your items.
+      </p>
+      <div className="flex items-center gap-2">
+        <Button asChild>
+          <Link href="/lists">Get Started</Link>
+        </Button>
+        <Button variant="ghost">Learn More</Button>
+      </div>
+    </main>
   );
 };
 
