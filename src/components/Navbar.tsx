eebd50caadf3a8995913afdc2a4cbb930794212a
amora7741 <mora7741@gmail.com>
2024-12-19 14:27:48 -0800

    Setup navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+import { FaShoppingCart } from "react-icons/fa";
+import { Button } from "./ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
+
+const Navbar = async () => {
+  const { userId } = await auth();
+
+  return (
+    <div className="flex w-full items-center justify-between p-4">
+      <Link href="/home" className="flex items-center gap-2">
+        <FaShoppingCart className="size-8" />
+        <span className="text-xl font-semibold">ShopMate</span>
+      </Link>
+      <div className="flex items-center gap-4">
+        <Button variant="ghost">Theme</Button>
+        {userId ? (
+          <UserButton showName />
+        ) : (
+          <Button asChild>
+            <Link href="login">Sign In</Link>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
